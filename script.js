cd4ed37a068f1bebc472856e2dfdac2399686244
Liliia Novikova <kplily85@gmail.com>
2020-10-25 17:43:23 +0200

    created change colors

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 import {colors} from "./colors.js"
 
+const refs = {
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+  body: document.querySelector('body')
+}
+// console.log(refs.body);
+// console.log(refs.startBtn);
+
+refs.startBtn.addEventListener('click', () => { changeBackgroundColor.start() })
+refs.stopBtn.addEventListener('click', () => { changeBackgroundColor.stop() })
+
+
+console.log(colors[0])
+
+const changeBackgroundColor = {
+  getNewColorId: null,
+  isActive: false,
+
+  start() {
+
+    if(this.isActive) {
+    return
+    }   
+    
+    this.isActive = true;
+    this.getNewColorId = setInterval(() => {
+      
+      refs.body.style.backgroundColor = `${colors[randomIntegerFromInterval(0, 5)]}`
+     
+      
+        }, 1000);
+},
+
+    stop() {
+      clearInterval(this.getNewColorId)
+    }
+}
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
+
 };
 
-console.log(randomIntegerFromInterval())
-
-document.body.style.backgroundColor = `colors`
-
-
-const changeBackgroundColor = {
-    start() {
-        const getNewColorId = setInterval(() => {
-            console.log("rtrtrtrt")
-        }, 1000);
-}
-
-}
